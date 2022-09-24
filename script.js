fc06de0b38608d6140c6f6bb7345830e415a46d9
diegocavalcante <diegocavalcante1984@gmail.com>
2022-09-24 14:34:34 -0300

    create banner and menu

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function Mclick(){
+  if(header_.style.opacity == '0'){
+    header_.style.opacity = '1'
+    icon.style.color = '#e550393c'
+  }
+  else{
+    header_.style.opacity = '0'
+    icon.style.color = '#FFF'
+    
+  }
+
+}
